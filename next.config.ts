787d000595d5b4ef25c799666bc5aa8cddec7093
CodexCos <varuas999@gmail.com>
2024-12-05 08:49:12 +0545

    test

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,16 @@
 module.exports = {
-  // async headers() {
-  //   return [
-  //     {
-  //       // Apply Cache-Control header to specific static assets like images
-  //       source: '/images/:path*',
-  //       headers: [
-  //         {
-  //           key: 'Cache-Control',
-  //           value: 'public, max-age=31536000, immutable', // 1 year for static assets
-  //         },
-  //       ],
-  //     },
-  //   ];
-  // },
+  async headers() {
+    return [
+      {
+        // Apply Cache-Control header to specific static assets like images
+        source: '/images/:path*',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, immutable', // 1 year for static assets
+          },
+        ],
+      },
+    ];
+  },
 };
