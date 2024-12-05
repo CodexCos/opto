740765aa3063dcfa2315496ce21d0a0dd090f89c
CodexCos <varuas999@gmail.com>
2024-12-05 09:02:47 +0545

    test

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,15 @@
 module.exports = {
-  // async headers() {
-  //   return [
-  //     {
-  //       source: '/images/:path*',
-  //       headers: [
-  //         {
-  //           key: 'Cache-Control',
-  //           value: 'public, max-age=31536000, immutable',
-  //         },
-  //       ],
-  //     },
-  //   ];
-  // },
+  async headers() {
+    return [
+      {
+        source: '/images/:path*',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, immutable',
+          },
+        ],
+      },
+    ];
+  },
 };
